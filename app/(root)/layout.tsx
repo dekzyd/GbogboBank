81e1c2dd3ad55a95198c07571f1c9d4d716ecb10
Adekoya <oladayo@spotter.ai>
2025-10-10 15:36:52 +0100

    implement mobile sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import Mobilenav from "@/components/Mobilenav";
 
 export default function RootLayout({
   children,
@@ -6,12 +8,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = {
-    firstName: 'Shendo', lastName: 'Hayi'
-  }
+    firstName: "Shendo",
+    lastName: "Hayi",
+  };
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn}/>
-      {children}
+      <Sidebar user={loggedIn} />
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <Mobilenav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
